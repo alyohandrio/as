7a7b11f165bff2456051125c79d2c913eba6716e
alyohandrio <alexdanilyu@gmail.com>
2023-12-25 03:28:50 +0300

    Update dataset download

--- download_dataset.sh
+++ download_dataset.sh
@@ -3,3 +3,7 @@ kaggle datasets download -d awsaf49/asvpoof-2019-dataset
 unzip -q ./asvpoof-2019-dataset.zip
 rm asvpoof-2019-dataset.zip
 rm -r PA
+rm LICENSE_text.txt
+rm README.txt
+rm asvspoof2019_Interspeech2019_submission.pdf
+rm asvspoof2019_evaluation_plan.pdf
